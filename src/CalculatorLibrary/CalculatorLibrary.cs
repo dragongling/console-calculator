04f657daf0a5c4f857ee3eddf82cf886623cc289
Alexander Slesarenko <dragongling@gmail.com>
2021-11-09 23:56:50 +0500

    fix: JSON now logs operation correctly

--- src/CalculatorLibrary/CalculatorLibrary.cs
+++ src/CalculatorLibrary/CalculatorLibrary.cs
@@ -46,19 +46,16 @@ namespace CalculatorLibrary
                     // Ask the user to enter a non-zero divisor.
                     if (num2 != 0)
                     {
-                        result = num1 / num2;
-                        writer.WriteValue("Divide");
+                        result = num1 / num2;                        
                     }
+                    writer.WriteValue("Divide");
                     break;
                 // Return text for an incorrect option entry.
                 default:
+                    writer.WriteValue("Unknown");
                     break;
             }
-            try
-            {
-                writer.WritePropertyName("Result");
-            }
-            catch (JsonWriterException) { }
+            writer.WritePropertyName("Result");
             writer.WriteValue(result);
             writer.WriteEndObject();
 
